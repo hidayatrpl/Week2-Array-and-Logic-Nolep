faa961cb68caaa043c300dca10168ee0a972a0f4
hidayatrpl <hidayatrpl1@gmail.com>
2025-05-27 19:49:20 +0700

    push soal2-4

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+function hitungJumlahKata(kalimat) {
+  let kata = kalimat.trim().split(/\s+/);
+
+  return kalimat.trim() === '' ? 0 : kata.length;
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
